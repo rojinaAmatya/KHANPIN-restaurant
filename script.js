dd81ae3deb86c41d6af09ff0c19647807832000c
Rojina Amatya <rojinaamatya1432@gmail.com>
2021-09-22 23:21:12 -0500

    created function for switching menus

--- script.js
+++ script.js
@@ -1,33 +1,46 @@
-let menu = document.querySelector('#menu-bar');
-let navbar = document.querySelector('.navbar');
+let menu = document.querySelector("#menu-bar");
+let navbar = document.querySelector(".navbar");
 
-menu.onclick = () =>{
+menu.onclick = () => {
+  menu.classList.toggle("fa-times");
+  navbar.classList.toggle("active");
+};
 
-  menu.classList.toggle('fa-times');
-  navbar.classList.toggle('active');
+window.onscroll = () => {
+  menu.classList.remove("fa-times");
+  navbar.classList.remove("active");
 
-}
-
-window.onscroll = () =>{
-
-  menu.classList.remove('fa-times');
-  navbar.classList.remove('active');
-
-  if(window.scrollY > 60){
-    document.querySelector('#scroll-top').classList.add('active');
-  }else{
-    document.querySelector('#scroll-top').classList.remove('active');
+  if (window.scrollY > 60) {
+    document.querySelector("#scroll-top").classList.add("active");
+  } else {
+    document.querySelector("#scroll-top").classList.remove("active");
   }
+};
 
+function loader() {
+  document.querySelector(".loader").classList.add("fade-out");
 }
 
-function loader(){
-  document.querySelector('.loader').classList.add('fade-out');
-}
-
-function fadeOut(){
+function fadeOut() {
   setInterval(loader, 3000);
 }
 
 window.onload = fadeOut();
 
+/*- function for menu switch */
+const menuTabs = document.querySelector(".menu-tabs");
+menuTabs.addEventListener("click", function (e) {
+  if (
+    e.target.classList.contains("menu-tab-items") &&
+    !e.target.classList.contains("active")
+  ) {
+    const target = e.target.getAttribute("data-target");
+    menuTabs.querySelector(".active").classList.remove("active");
+    e.target.classList.add("active");
+    const menuSection = document.querySelector(".menu-section");
+    menuSection
+      .querySelector(".menu-tab-content.active")
+      .classList.remove("active");
+    menuSection.querySelector(target).classList.add("active");
+  }
+});
